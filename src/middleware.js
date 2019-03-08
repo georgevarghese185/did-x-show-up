d6e0987f5f5bc4563d81ae8136e091935cbd0a3a
George Varghese M <georgevarghese185@yahoo.com>
2019-03-08 19:20:55 +0530

    Add urlencoded parser

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,6 @@
 const morgan = require('morgan');
 const jsonParser = require('body-parser').json();
+const urlEncodedParser = require('body-parser').urlencoded();
 
 const requestLogger = (req, resp, next) => {
   console.log(`\n\nRequest body:\n\n${JSON.stringify(req.body, null, 2)}\n\n`);
@@ -9,6 +10,7 @@ const requestLogger = (req, resp, next) => {
 const setupMiddleware = (app) => {
   app.use(morgan('dev'));
   app.use(jsonParser);
+  app.use(urlEncodedParser);
   app.use(requestLogger);
 }
 
