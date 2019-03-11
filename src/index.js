96f5b5b3098e51ee4a8a6c9dc52832916d0e6ec5
George Varghese M <georgevarghese185@yahoo.com>
2019-03-11 13:46:19 +0530

    Take port from environment if provided

--- src/index.js
+++ src/index.js
@@ -13,14 +13,16 @@ const start = async (app) => {
 
   let serverConfig = await getServerConfig();
 
+  const port = serverConfig.port || process.env.PORT || 4000;
+
   console.log("\nSetting up Express");
   Middleware(app, sequelize);
   Routes(app, {sequelize, models, serverConfig});
 
   console.log("\nStarting Server");
-  await new Promise((resolve, reject) => {app.listen(serverConfig.port || 4000, resolve)});
+  await new Promise((resolve, reject) => {app.listen(port, resolve)});
   console.log("\nServer Started!");
-  console.log(`\n\nListening on ${serverConfig.port || 4000}`);
+  console.log(`\n\nListening on ${port}`);
 }
 
 start(app)
